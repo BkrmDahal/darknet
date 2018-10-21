8741da869a231663b8d00e07b1839b822f08c9f9
Bikram Dahal <bkrm.dahal@gmail.com>
2018-10-21 23:02:45 +0545

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+# https://medium.com/@manivannan_data/how-to-train-yolov3-to-detect-custom-objects-ccbcafeb13d2
+echo "make file"
+make
+
+echo "start training"
+./darknet detector train cfg/obj.data cfg/invoice-yolov3.cfg darknet53.conv.74
